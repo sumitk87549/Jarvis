6c0b4e06d07c365db9ff60ae9b553a7a142a975d
Sumit Kumar <sumitk87549@gmail.com>
2023-02-15 13:09:46 +0530

    Speak and Listen Function Implemented
    speak using pyttsx3
    listen using speech_recognition
    speech recognizer - recognize_google

---
+++ jarvis.py
@@ -0,0 +1,52 @@
+import pyttsx3 as pytts
+import speech_recognition
+import time
+
+text_to_speech_engine = pytts.init('sapi5') # sapi5 is TTS engine for windows; other TTS are 'nsss' for MacOS & 'espeak' for linux and other os
+
+voices = text_to_speech_engine.getProperty('voices')
+# print(voices) # Print availabe voices
+# rate = text_to_speech_engine.getProperty('rate')
+# print(rate) # Print current speech rate 
+# volume = text_to_speech_engine.getProperty('volume')
+# print(volume) # Print current speech volume
+
+text_to_speech_engine.setProperty('voices', voices[1].id) # index value can be changed for different types of voices available
+text_to_speech_engine.setProperty('rate', 231) 
+text_to_speech_engine.setProperty('volume', 1.0)
+
+
+def speak(audio):
+    print("-------------------------------------->>> ~~~~~~~ <<<--------------------------------------")
+    text_to_speech_engine.say(audio)
+    print(f": {audio}")
+    text_to_speech_engine.runAndWait()
+    print("-------------------------------------->>> ~~~~~~~ <<<--------------------------------------")
+    
+def listenCommand():
+    listener = speech_recognition.Recognizer()
+    with speech_recognition.Microphone() as source:
+        # print("Setting up...")
+        # listener.adjust_for_ambient_noise(source, duration=.5) # In this line system will take 1 sec to adjust itself for environment noise  
+        listener.pause_threshold = 0.7 # number of seconds the system will take to recognize the voice once the user has stopped speaking
+        print("Listening...")
+        audio = listener.listen(source)
+        try:
+            print("Recognizing...")
+            recognized_speech = listener.recognize_google(audio, language='en-in')
+            print(f": {recognized_speech}")
+        except Exception as error:
+            print(error)
+            return "none"
+        return recognized_speech.lower()
+            
+        
+
+speak('Hello. I am Jarvis. Tony Stark\'s personal virtual assistant.')    
+# command = listenCommand()
+
+# if 'jarvis' in command:
+#     speak("Yes Sir. I am alive aware and ready to follow your command.")
+# else:
+#     speak("sorry sir! unable to follow...")
+
